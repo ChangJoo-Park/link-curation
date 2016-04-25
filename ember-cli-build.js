db9d28b1320ce3daaa73e1880e2805bd54ec471b
ChangJoo Park <changjoo.park.cj@gmail.com>
2016-04-25 22:28:36 +0200

    Add and tested notification / sweetalert

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,7 +20,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import("bower_components/bulma/css/bulma.css");
-  app.import("node_modules/sweetalert/dist/sweetalert.min.js");
-  app.import("node_modules/sweetalert/dist/sweetalert.css");
+  app.import("bower_components/sweetalert/dist/sweetalert.min.js");
+  app.import("bower_components/sweetalert/dist/sweetalert.css");
   return app.toTree();
 };
